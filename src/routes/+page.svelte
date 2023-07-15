44a8e698d814d16a3e9e46c5094599c716b482d6
jaksia <jakub.siagi@gmail.com>
2023-07-15 21:25:55 +0000

    Add comming soon message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-<h1>jaksia</h1>
+<div class="h-screen w-full flex flex-row justify-center items-center bg-gradient-to-tl from-violet-500 to-fuchsia-600">
+    <div class="flex flex-col justify-between items-center px-16 py-10 from-sky-500 to-indigo-700 bg-gradient-to-br rounded-3xl">
+	<h1 class="text-4xl text-gray-100 font-bold">Comming soon</h1>
+	<div class="flex flex-col pt-10 text-slate-200 text-center space-y-1">
+	    <span>At some point in time a personal page will appear here.</span>
+	    <span>It will probably be a while before that happens<br>as I don't have much spare free time.</span>
+	</div>
+    </div> 
+</div>
